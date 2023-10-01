40a360e1b8038ae3247701ac5e8b3faf1dfa0b93
Siddhant Singh <singhsiddhantakshat@gmail.com>
2023-10-01 18:14:59 -0500

    feat: updates.

--- siddhantwebsite/app/api/get_currently_streaming_from_spotify/route.ts
+++ siddhantwebsite/app/api/get_currently_streaming_from_spotify/route.ts
@@ -1,3 +1,4 @@
+"use client";
 import { NextResponse } from "next/server";
 import fetch from "node-fetch"; // node-fetch is more suitable for server-side
 
@@ -5,27 +6,27 @@ import fetch from "node-fetch"; // node-fetch is more suitable for server-side
 require("dotenv").config({ path: "../../../.env" });
 
 export async function GET() {
-	// Fetch the access token from your other route
-	// Replace this URL with the correct URL of your deployed function
-	const tokenRes = await fetch(
-		`${process.env.site}/api/get_access_token_from_spotify`
-	);
-	const tokenData: any = await tokenRes.json();
-	const accessToken = tokenData.accessToken;
+  // Fetch the access token from your other route
+  // Replace this URL with the correct URL of your deployed function
+  const tokenRes = await fetch(
+    `${process.env.site}/api/get_access_token_from_spotify`,
+  );
+  const tokenData: any = await tokenRes.json();
+  const accessToken = tokenData.accessToken;
 
-	// Fetch the currently playing song from Spotify
-	const res = await fetch(
-		"https://api.spotify.com/v1/me/player/currently-playing",
-		{
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		}
-	);
+  // Fetch the currently playing song from Spotify
+  const res = await fetch(
+    "https://api.spotify.com/v1/me/player/currently-playing",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
 
-	if (res.ok) {
-		const data = await res.json();
-		/*
+  if (res.ok) {
+    const data = await res.json();
+    /*
 		DONE
 		to extract:
 		1. from the album
@@ -34,17 +35,23 @@ export async function GET() {
 		2. from the artists
 			All of the names of the people on the song 
 		*/
-		let out = { title: "", "image-url": "", artists: "", "song-url": "" }; // song ~ (data as any)["item"]["external_urls"]["spotify"]
-		out["title"] = (data as any)["item"]["name"];
-		out["image-url"] = (data as any)["item"]["album"]["images"][0];
-		out["artists"] = (data as any)["item"]["album"]["artists"].map((e: any) => {
-			return e["name"];
-		});
-		out["song-url"] = (data as any)["item"]["external_urls"]["spotify"];
-		console.log(out);
-		return NextResponse.json({ status: "OK", out });
-	} else {
-		console.log("Error:", res.status);
-		return NextResponse.json({ status: "NOTOK" });
-	}
+    let out = { title: "", "image-url": "", artists: "", "song-url": "" }; // song ~ (data as any)["item"]["external_urls"]["spotify"]
+    try {
+      out["title"] = (data as any)["item"]["name"];
+      out["image-url"] = (data as any)["item"]["album"]["images"][0];
+      out["artists"] = (data as any)["item"]["album"]["artists"].map(
+        (e: any) => {
+          return e["name"];
+        },
+      );
+      out["song-url"] = (data as any)["item"]["external_urls"]["spotify"];
+      console.log(out);
+      return NextResponse.json({ status: "OK", out });
+    } catch (e) {
+      return NextResponse.json({ status: "NOTOK" });
+    }
+  } else {
+    console.log("Error:", res.status);
+    return NextResponse.json({ status: "NOTOK" });
+  }
 }
--- siddhantwebsite/app/components/AboutMe.tsx
+++ siddhantwebsite/app/components/AboutMe.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const AboutMe = () => {
   return (
-    <section className=" outline outline-black rounded-lg p-5 hover:ease-linear hover:duration-150">
+    <section className="outline outline-black rounded-lg p-5 hover:ease-linear hover:duration-150">
         <div className='text-2xl underline'>Siddhant Singh</div>
         <div className="text-xl">welcome to my slice of the internet! i&apos;m an 
         ambitious 17 year old looking to take asymetric risks and make a difference on this world.</div>
